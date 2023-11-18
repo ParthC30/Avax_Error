d35cc128ad26db3bc1bdd1f774096631810576ce
ParthC30 <132546773+ParthC30@users.noreply.github.com>
2023-11-18 19:54:27 +0530

    Update Error.sol

--- Error.sol
+++ Error.sol
@@ -1,21 +1,39 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-contract Error {
-    
-    function testRequire(uint _i) public pure {
-        require(_i > 10, "Input must be greater than 10");
+contract ATM_MACHINE {
+
+    address owner;
+    mapping (address => uint) public balance;
+
+    modifier onlyOwner {
+        require(msg.sender == owner, "Only Owner can call this function");
+        _;
     }
 
-    function testRevert(uint _i) public pure {
-        if (_i <= 10) {
-            revert("Input must be greater than 10");
+    constructor() {
+        owner = msg.sender;
+        balance[msg.sender] += 100;
+    }
+
+
+    function AddMoney(uint g) public {
+        assert(g > 10);
+        balance[msg.sender] += g;
+    }
+
+    function Withdraw(uint p) public {
+        require(p <= balance[msg.sender],"You don't have enough money to withdraw");
+        balance[msg.sender] -= p;
+    }
+
+    function Transfer(uint t, address _rec) public {
+        if(msg.sender != owner){
+            revert("You are not the owner");
+        }else {
+            balance[_rec] += t;
+            balance[msg.sender] -= t;
         }
-    }
 
-    function testAssert(uint num) pure public {
-        assert(num > 5);
     }
-
 }
-
